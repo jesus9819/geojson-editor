08e4c17fd34fa67a515cef3363c9a511ec3f88b5
Jesus Jose Zapata Orozco <60452287+jesus9819@users.noreply.github.com>
2025-09-20 16:14:46 -0500

    chore(app): bootstrap MapEditorComponent as root

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { MapEditorComponent } from './app/components/map-editor/map-editor.component';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(MapEditorComponent)
   .catch((err) => console.error(err));
